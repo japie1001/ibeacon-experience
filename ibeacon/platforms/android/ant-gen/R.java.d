b7ec1c72d3c25114de2ef502872e7c985df551de
Jaap <woltjes@gmail.com>
2014-08-23 17:46:33 +0200

    added push notification plugin

--- ibeacon/platforms/android/ant-gen/R.java.d
+++ ibeacon/platforms/android/ant-gen/R.java.d
@@ -6,4 +6,9 @@ c:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experienc
 c:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experience\ibeacon\platforms\android\res\drawable-xhdpi\icon.png \
 c:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experience\ibeacon\platforms\android\res\values\strings.xml \
 c:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experience\ibeacon\platforms\android\res\xml\config.xml \
+c:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experience\ibeacon\platforms\android\ant-build\res\drawable\icon.png \
+c:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experience\ibeacon\platforms\android\ant-build\res\drawable-hdpi\icon.png \
+c:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experience\ibeacon\platforms\android\ant-build\res\drawable-ldpi\icon.png \
+c:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experience\ibeacon\platforms\android\ant-build\res\drawable-mdpi\icon.png \
+c:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experience\ibeacon\platforms\android\ant-build\res\drawable-xhdpi\icon.png \
 c:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experience\ibeacon\platforms\android\ant-build\AndroidManifest.xml \
