db08ac1cfd919b73d0dea13e8b692078b36dafa9
Jaap <woltjes@gmail.com>
2014-08-23 20:32:55 +0200

    replaced SENUI with PUSH Notification
    changed author ID
    
    WORKING!

--- ibeacon/platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
+++ ibeacon/platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
@@ -2,3 +2,5 @@ c:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experienc
 c:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experience\ibeacon\platforms\android\ant-build\HelloWorld.ap_ \
 c:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experience\ibeacon\platforms\android\ant-build\classes.dex \
 C:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experience\ibeacon\platforms\android\CordovaLib\ant-build\classes.jar \
+c:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experience\ibeacon\platforms\android\libs\gcm.jar \
+c:\Users\jwoltjes\Documents\iBeacon Experience\the code\master\ibeacon-experience\ibeacon\platforms\android\libs\android-support-v13.jar \
--- ibeacon/plugins/com.phonegap.plugins.PushPlugin/Example/server/pushGCM.rb
+++ ibeacon/plugins/com.phonegap.plugins.PushPlugin/Example/server/pushGCM.rb
@@ -1,9 +1,12 @@
 require 'rubygems'
 require 'pushmeup'
+
+ENV['SSL_CERT_FILE'] = 'C:\Ruby200\lib\ruby\site_ruby\2.0.0\rubygems\ssl_certs\cacert.pem'
+
 GCM.host = 'https://android.googleapis.com/gcm/send'
 GCM.format = :json
-GCM.key = "API_KEY_GOES_HERE"
-destination = ["REGISTRATION_ID_GOES_HERE"]
+GCM.key = "AIzaSyACEZKYKbSK5-6lQnDn63ZihYiiH1PWowQ"
+destination = ["APA91bHz4Ecyd1iiPjdIcqN4LL78F1tvpvzaW__RZMHficNmXfk34uitB34Wv33NhCnurxLw6rn_jNF8L-VZyTdOfhm-5-4-VlHlcrawc_9MSMXgVT3P6Nep5nI11P_Zn6gJGA6tTo2u0XjO63IB0pq5m4qXp5gYDw"]
 data = {:message => "PhoneGap Build rocks!", :msgcnt => "1", :soundname => "beep.wav"}
 
 GCM.send_notification( destination, data)
